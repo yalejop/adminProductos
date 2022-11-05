a770a3e45a844e9056f70138af133c5ce2e2d715
Alejandro Cataño Herrera <alejo.9211@hotmail.com>
2022-11-05 11:47:56 -0500

    listar y detallar productos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import TodosProductos from './components/TodosProductos';
+import Producto from './components/Producto';
+import NuevoProducto from './components/NuevoProducto';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact render={()=> <TodosProductos/>} />
+          <Route path="/producto/:id" exact render={()=> <Producto/>} />
+          <Route path="/nuevo" render={()=> <NuevoProducto/>} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
